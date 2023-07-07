b67155c70a56a86cd76e44ef0a107737a80b874b
nicolas3537 <nic.auzemery@orange.fr>
2023-07-07 18:32:05 +0000

    authentification done

---
+++ website/todoapp/usersapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+	if request.method == 'POST' :
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()		
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			login(request,user)	
+			messages.success(request, f'Coucou {username}, Votre compte a été créé avec succès !')					
+			return redirect('home')
+	else :
+		form = UserRegistrationForm()
+	return render(request,'registration/register.html',{'form' : form})
