b2d63458c934f2d466e53130fc08c82b775ebe3b
nicolas3537 <nic.auzemery@orange.fr>
2023-07-09 14:55:15 +0000

    application_Todolist_ok

--- website/todoapp/todolist/urls.py
+++ website/todoapp/todolist/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('todolist/', views.todolist, name="todolist"),
+    path('update/<int:pk>/', views.update, name="update"),
+    path("delete/<int:pk>/", views.delete, name="delete"),
 ]
--- website/todoapp/todolist/views.py
+++ website/todoapp/todolist/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from django.contrib.auth.decorators import login_required
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+@login_required
+def todolist(request):
+    form = TaskForm()
+    
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        print("test")
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author =request.user
+            task.save()
+            return redirect("todolist")
+    
+    
+    tasks = Task.objects.filter(author=request.user)
+    return render(request, "todolist.html", {"tasks": tasks, "task_form":form})
+
+def update(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance= task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid :
+            form.save()
+            return redirect("todolist")
+    return render(request, "update.html", {"edit_task_form": form})
+
+def delete(request,pk):
+    task = Task.objects.get(id=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect("todolist")
+    return render(request, "delete.html", {"task":task})
